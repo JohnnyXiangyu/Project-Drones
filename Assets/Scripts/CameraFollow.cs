6af37fcbf4a747e3b4258a59b6d142b34d1cf298
Xiangyu Wan <johnnywon@g.ucla.edu>
2021-05-10 14:25:35 -0700

    removed ecs from project

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,20 +1,20 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.Entities;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.Entities;
 
-public class CameraFollow : MonoBehaviour
-{
-    public static CameraFollow instance = null;
+//public class CameraFollow : MonoBehaviour
+//{
+//    public static CameraFollow instance = null;
 
-    private void Awake() {
-        if (instance == null)
-            instance = this;
-        else if (instance != this)
-            Destroy(gameObject);
-    }
+//    private void Awake() {
+//        if (instance == null)
+//            instance = this;
+//        else if (instance != this)
+//            Destroy(gameObject);
+//    }
 
-    public void SetObjPosition(Vector3 pos) {
-        transform.position = pos + new Vector3(0, 80, 0);
-    }
-}
+//    public void SetObjPosition(Vector3 pos) {
+//        transform.position = pos + new Vector3(0, 80, 0);
+//    }
+//}
