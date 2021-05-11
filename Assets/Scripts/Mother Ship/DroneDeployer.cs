54461395693ed61b1ead131cd4bbda7aa84f870d
Xiangyu Wan <johnnywon@g.ucla.edu>
2021-05-11 02:25:53 -0700

    implemented partially drone deploy system: there's still need to connect the deployer with a manufacturer to fully enable a demo

--- Assets/Scripts/Mother Ship/DroneDeployer.cs
+++ Assets/Scripts/Mother Ship/DroneDeployer.cs
@@ -3,21 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DroneDeployer : FirableBase {
+    // settings
+    [SerializeField]
+    KeyCode makeKey = KeyCode.Space;
+    [SerializeField]
+    Transform spawnPoint;
+
     int currentDrone = 0;
     List<GameObject> dronePrefabs = new List<GameObject>();
     List<List<GameObject>> droneInstances = new List<List<GameObject>>();
     
     protected override void Update() {
-        base.Update();
+        // switch drone slot
+        // loop each possible drone selection
+        for (int i = 0; i < dronePrefabs.Count; i ++) {
+            // take input (alpha1 + i => the i_th drone preset)
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                currentDrone = i;
+            }
+        }
 
-        // TODO: switch drone slot
+        // TODO: drone manufacturing
+        if (Input.GetKeyDown(makeKey)) {
+            // not implemented
+        }
+
+        base.Update();
     }
 
     protected override void PrimaryFire(Vector3 point, GameObject obj) {
-        
+        // spawn a drone and give it attack-ish order
+        if (droneInstances[currentDrone].Count > 0) {
+            GameObject newDeploy = droneInstances[currentDrone][-1];
+
+            Command newCommand = new Command();
+            newCommand.clickedObj = obj;
+            newCommand.position = point;
+            newCommand.motherShip = gameObject;
+
+            if (obj.GetComponent<IAttackable>() != null) {
+                newCommand.type = Command.CommandType.CHASE;
+            }
+            
+            newDeploy.GetComponent<DroneBase>().Deploy(newCommand);
+            newDeploy.transform.position = spawnPoint.position;
+        }
     }
 
     protected override void SecondaryFire(Vector3 point, GameObject obj) {
-        
+        // spawn a drone and give it secure order
+        if (droneInstances[currentDrone].Count > 0) {
+            GameObject newDeploy = droneInstances[currentDrone][-1];
+
+            Command newCommand = new Command() {
+                clickedObj = obj,
+                position = point,
+                motherShip = gameObject,
+                type = Command.CommandType.SECURE
+            };
+
+            newDeploy.GetComponent<DroneBase>().Deploy(newCommand);
+            newDeploy.transform.position = spawnPoint.position;
+        }
     }
 }
